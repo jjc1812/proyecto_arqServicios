8a7ba76426dae6dc0eef3b2cf566a554c6eba909
jjc1812 <josejcoronel98@gmail.com>
2023-05-15 20:23:30 -0300

    base de datos

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'poo'
+})
 
 app.get("/", (req, res) => {
     res.send("Hello Word!");
@@ -9,3 +16,21 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`El servidor esta andando en ${port}`);
 })
+
+connection.connect(function(err) {
+    if(err){
+        console.error(err.stack);
+        return;
+    }
+    console.log("State " + connection.state);
+});
+
+connection.query("SELECT * FROM poo.autos;", function (error, results){
+    if (error){
+        console.error(error)
+    }
+    
+    results.forEach(elemento => {
+        console.log(elemento);
+    })
+});
